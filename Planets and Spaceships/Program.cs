388dec14674e451d3850f05a712dfa3301d1e9d7
Liza Star <lizadstar18@gmail.com>
2020-08-19 19:50:02 -0500

    Add project files.

---
+++ Planets and Spaceships/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planets_and_Spaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            Console.WriteLine($"{string.Join(",", planetList)}");
+            var lastTwoPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastTwoPlanets);
+
+            Console.WriteLine($"{string.Join(",", planetList)}");
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            Console.WriteLine($"{string.Join(",", planetList)}");
+
+            planetList.Add("Pluto");
+
+            Console.WriteLine($"{string.Join(",", planetList)}");
+
+            var rockyPlanets = new List<string>(planetList.GetRange(0, 3));
+
+            Console.WriteLine($"Rocky Planets: {string.Join(",", rockyPlanets)}");
+
+            planetList.Remove("Pluto");
+
+            Console.WriteLine($"Removed Pluto: {string.Join(",", planetList)}");
+
+            var Spaceship = new Dictionary<string, List<string>>
+            {
+                {
+                    "Voyager 1", new List<string> {"Mercury", "Jupiter", "Mars"}
+                },
+                {
+                    "Voyager 2", new List<string> {"Mercury", "Earth", "Mars"}
+                },
+                {
+                    "Mariner 3", new List<string> {"Saturn", "Jupiter", "Uranus"}               
+                },
+                {
+                    "Mariner 10", new List<string> {"Neptune", "Venus", "Pluto"}
+                }
+            };
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine($"{planet}:");
+
+                foreach (var (probe, planets) in Spaceship)
+                {
+                    foreach (var landedOnPlant in planets)
+                    {
+                        if (planet == landedOnPlant)
+                        {
+                            Console.WriteLine($"{probe}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
